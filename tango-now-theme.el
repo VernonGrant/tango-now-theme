7ca015dfe00754f09e3563c12738a9d6aa05264a
Vernon Grant <vernon@ruppell.io>
2022-06-03 21:39:36 +0800

    Bumped version number

--- tango-now-theme.el
+++ tango-now-theme.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2022 , Vernon Grant
 
 ;; Author: Vernon Grant
-;; Version: 0.1
+;; Version: 1.0
 ;; Package-Requires: ((emacs "24"))
 ;; Created with ThemeCreator, https://github.com/mswift42/themecreator.
 
